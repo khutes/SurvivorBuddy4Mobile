c7f04962add8e55672640b4555f4b7d95cea81a6
khutes <kyle.hutto3@gmail.com>
2020-10-30 13:26:29 -0500

    Added back button to BuddyAudioActivity

--- app/src/main/java/com/example/survivorbuddy4mobile/BuddyAudioService.java
+++ app/src/main/java/com/example/survivorbuddy4mobile/BuddyAudioService.java
@@ -95,8 +95,6 @@ public class BuddyAudioService extends Service {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-
     }
 
     public void autoRunThreads() {
