b98f7171e5d8d9af00b4ef24fde8f91ef8437476
khutes <kyle.hutto3@gmail.com>
2020-11-16 01:46:25 -0600

    Added port number validation and toast messages for applied settings

--- app/src/main/java/com/example/survivorbuddy4mobile/SettingsActivity.java
+++ app/src/main/java/com/example/survivorbuddy4mobile/SettingsActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -64,13 +65,61 @@ public class SettingsActivity extends AppCompatActivity {
 
     }
 
-    public void applySettings(View view) {
+    private boolean validatePortNum(int portNum) {
+        Log.i(TAG, "validatingPort");
+        if(portNum >= 1 && portNum <= 65535) {
+            Log.i(TAG, "portGood");
+            return true;
+        }
+        return false;
+    }
 
+    public void applySettings(View view) {
+        Log.i(TAG, "applySettings");
         mEditor = mPreferences.edit();
-        mEditor.putInt("rtspPort", Integer.parseInt(String.valueOf(rtspPortInput.getText())));
-        mEditor.putInt("audioPort", Integer.parseInt(String.valueOf(audioPortInput.getText())));
-        mEditor.putInt("messagePort", Integer.parseInt(String.valueOf(messagePortInput.getText())));
-        mEditor.commit();
+        int rtsp_num = 0;
+        int audio_num = 0;
+        int message_num = 0;
+        boolean parseGood = true;
+        try {
+            rtsp_num = Integer.parseInt((String.valueOf(rtspPortInput.getText())));
+            audio_num = Integer.parseInt((String.valueOf(audioPortInput.getText())));
+            message_num = Integer.parseInt((String.valueOf(messagePortInput.getText())));
+        } catch (NumberFormatException e) {
+            Toast.makeText(SettingsActivity.this, "One or more invalid ports", Toast.LENGTH_SHORT).show();
+            parseGood = false;
+        }
+
+        if(parseGood) {
+
+            boolean allApplied = true;
+
+            if (validatePortNum(rtsp_num)) {
+                mEditor.putInt("rtspPort", rtsp_num);
+            } else {
+                allApplied = false;
+                Toast.makeText(SettingsActivity.this, "Invalid Phone to PC AV Port", Toast.LENGTH_SHORT).show();
+            }
+
+            if (validatePortNum(audio_num)) {
+                mEditor.putInt("audioPort", audio_num);
+            } else {
+                allApplied = false;
+                Toast.makeText(SettingsActivity.this, "Invalid PC to Phone Audio Port", Toast.LENGTH_SHORT).show();
+            }
+
+            if (validatePortNum(message_num)) {
+                mEditor.putInt("messagePort", message_num);
+            } else {
+                allApplied = false;
+                Toast.makeText(SettingsActivity.this, "Invalid PC to Phone Message Port", Toast.LENGTH_SHORT).show();
+            }
+
+            mEditor.commit();
+            if (allApplied) {
+                Toast.makeText(SettingsActivity.this, "Applied", Toast.LENGTH_SHORT).show();
+            }
+        }
 
     }
 
